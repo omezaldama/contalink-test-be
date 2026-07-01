4a811ac1722a1722a15f751273136a6fc2b10f6f
Oscar Meza Aldama <oscar_meza5@hotmail.com>
2026-07-01 12:53:14 -0600

    Changed dates to be received as query params instead of body

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,7 +1,8 @@
+from datetime import date
+
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 
-from app.api.schemas.date_schemas import DateRange
 from app.api.schemas.invoice_schemas import GetInvoicesResponse
 from app.db.session import get_session
 from app.services.get_invoices_service import InvoicesService
@@ -12,12 +13,13 @@ router = APIRouter(prefix='/api')
 @router.get(
     '/invoices',
     response_model=GetInvoicesResponse,
-    status_code=status.HTTP_200_OK
+    status_code=status.HTTP_200_OK,
 )
 def get_invoices(
     *,
     session: Session = Depends(get_session),
-    date_range: DateRange,
+    from_date: date,
+    to_date: date,
 ):
-    invoices = InvoicesService(session).get_by_date_range(date_range.from_date, date_range.to_date)
+    invoices = InvoicesService(session).get_by_date_range(from_date, to_date)
     return {'invoices': invoices}
--- app/api/schemas/date_schemas.py
+++
@@ -1,8 +0,0 @@
-from datetime import date
-
-from pydantic import BaseModel
-
-
-class DateRange(BaseModel):
-    from_date: date
-    to_date: date
